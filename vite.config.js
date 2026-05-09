eeb660b2243ab63b01d075f9f9905c7cd7f2a504
Reyansh Negi <reyanshnegi.tech@gmail.com>
2026-05-09 21:20:32 +0530

    Remove server settings from vite.config.js
    Removed server configuration from Vite config.

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    host: '127.0.0.1',
-    port: 5176,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8080',
-        changeOrigin: true
-      }
-    }
-  }
 });
